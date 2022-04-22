b741f138b20f52c15443814a37c0483640ee93dc
Anton Malygin <ai.malygin@icloud.com>
2022-04-22 15:39:34 +0400

    Initial Commit

---
+++ Exchanges/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exchanges
+//
+//  Created by Anton Malygin on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
